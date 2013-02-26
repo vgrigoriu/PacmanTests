6f8d429d35770e6feb47af51e22e9f9f47cefe5f
Victor Grigoriu <vgrigoriu@gmail.com>
2013-02-26 20:50:14 +0200

    Pacman moves

--- PacmanTests/Class1.cs
+++ PacmanTests/Class1.cs
@@ -11,35 +11,97 @@ namespace PacmanTests
             var gameState = new GameState();
             gameState.pacmanPosition = "a5";
 
-            gameState.clockTick++;
-            gameState.pacmanPosition = "a4";
+            MovePacman(gameState);
 
             Assert.Equal("a4", gameState.pacmanPosition);
         }
 
         [Fact]
-        public void GhostIsMovingCorrectlyAfterFirstPosition()
+        public void GhostIsMovingCorrectly()
         {
             var gameState = new GameState();
-            gameState.ghostPosition = "i1";
+            gameState.ghostPosition = "a3";
 
-            gameState.clockTick++;
-            gameState.ghostPosition = "i2";
+            MoveGhost(gameState);
 
-            Assert.Equal("i2", gameState.ghostPosition);
+            Assert.Equal(gameState.ghostMoves["a3"], gameState.ghostPosition);
         }
 
         [Fact]
-        public void GhostMovesCorrectlyAfterSecondTick()
+        public void PacmanReversesDirectionAtEndOfForwardPath()
         {
             var gameState = new GameState();
+            gameState.pacmanPosition = "i1";
+
+            MovePacman(gameState);
+            Assert.False(gameState.pacmanIsMovingForward);
+        }
+
+        [Fact]
+        public void WhenMovingBackwardsPacmanUsesBackwardPath()
+        {
+            var gameState = new GameState();
+            gameState.pacmanIsMovingForward = false;
+            gameState.pacmanPosition = "f5";
+
+            MovePacman(gameState);
+
+            Assert.Equal(gameState.pacmanBackwardMoves["f5"], gameState.pacmanPosition);
+        }
+
+        [Fact]
+        public void GhostBecomesToxicAtTheCorrectTick()
+        {
+            var gameState = new GameState();
+            gameState.toxicTickCount = 2;
+            gameState.clockTick = 0;
+            gameState.ghostPosition = "a1";
+
+            MoveGhost(gameState);
+
+            Assert.True(gameState.ghostIsToxic);
+        }
+
+        [Fact]
+        public void GhostCeasesToBeToxicAfterOneTick()
+        {
+            var gameState = new GameState();
+            gameState.toxicTickCount = 2;
             gameState.clockTick = 1;
-            gameState.ghostPosition = "i2";
+            gameState.ghostPosition = "a2";
+            gameState.ghostIsToxic = true;
 
+            MoveGhost(gameState);
+
+            Assert.False(gameState.ghostIsToxic);
+        }
+
+        private void MoveGhost(GameState gameState)
+        {
             gameState.clockTick++;
-            gameState.ghostPosition = "i3";
+            gameState.ghostPosition = gameState.ghostMoves[gameState.ghostPosition];
+            gameState.ghostIsToxic = (gameState.clockTick + 1) % gameState.toxicTickCount == 0;
+        }
 
-            Assert.Equal("i3", gameState.ghostPosition);
+        private void MovePacman(GameState gameState)
+        {
+            gameState.clockTick++;
+
+            var pacmanMoves = PacmanMoves(gameState);
+            if (!pacmanMoves.ContainsKey(gameState.pacmanPosition))
+            {
+                gameState.pacmanIsMovingForward = !gameState.pacmanIsMovingForward;
+                pacmanMoves = PacmanMoves(gameState);
+            }
+
+            gameState.pacmanPosition = pacmanMoves[gameState.pacmanPosition];
+        }
+
+        private static Dictionary<string, string> PacmanMoves(GameState gameState)
+        {
+            return gameState.pacmanIsMovingForward
+                       ? gameState.pacmanForwardMoves
+                       : gameState.pacmanBackwardMoves;
         }
 
         private class GameState
@@ -48,8 +110,85 @@ namespace PacmanTests
 
             public string pacmanPosition;
             public string ghostPosition;
+            public int toxicTickCount = 2;
+            public bool ghostIsToxic;
+            public bool pacmanIsMovingForward = true;
 
-            public List<string> ghostPath = new List<string> { "i1", "i2", "i3" };
+            public Dictionary<string, string> ghostMoves = new Dictionary<string, string>()
+                                                               {
+                                                                   {"i1", "i2"},
+                                                                   {"i2", "i3"},
+                                                                   {"i3", "i4"},
+                                                                   {"i4", "i5"},
+                                                                   {"i5", "h5"},
+                                                                   {"h5", "g5"},
+                                                                   {"g5", "f5"},
+                                                                   {"f5", "e5"},
+                                                                   {"e5", "d5"},
+                                                                   {"d5", "c5"},
+                                                                   {"c5", "b5"},
+                                                                   {"b5", "a5"},
+                                                                   {"a5", "a4"},
+                                                                   {"a4", "a3"},
+                                                                   {"a3", "a2"},
+                                                                   {"a2", "a1"},
+                                                                   {"a1", "b1"},
+                                                                   {"b1", "c1"},
+                                                                   {"c1", "d1"},
+                                                                   {"d1", "e1"},
+                                                                   {"e1", "f1"},
+                                                                   {"f1", "g1"},
+                                                                   {"g1", "h1"},
+                                                                   {"h1", "i1"},
+                                                               };
+
+            public Dictionary<string, string> pacmanForwardMoves = new Dictionary<string, string>()
+                                                                {
+                                                                    {"a5", "a4"},
+                                                                    {"a4", "a3"},
+                                                                    {"a3", "a2"},
+                                                                    {"a2", "a1"},
+                                                                    {"a1", "b1"},
+                                                                    {"b1", "c1"},
+                                                                    {"c1", "d1"},
+                                                                    {"d1", "e1"},
+                                                                    {"e1", "e2"},
+                                                                    {"e2", "e3"},
+                                                                    {"e3", "e4"},
+                                                                    {"e4", "e5"},
+                                                                    {"e5", "f5"},
+                                                                    {"f5", "g5"},
+                                                                    {"g5", "h5"},
+                                                                    {"h5", "i5"},
+                                                                    {"i5", "i4"},
+                                                                    {"i4", "i3"},
+                                                                    {"i3", "i2"},
+                                                                    {"i2", "i1"},
+                                                                };
+
+            public Dictionary<string, string> pacmanBackwardMoves = new Dictionary<string, string>()
+                                                                        {
+                                                                            {"i1", "i2"},
+                                                                            {"i2", "i3"},
+                                                                            {"i3", "i4"},
+                                                                            {"i4", "i5"},
+                                                                            {"i5", "h5"},
+                                                                            {"h5", "g5"},
+                                                                            {"g5", "f5"},
+                                                                            {"f5", "e5"},
+                                                                            {"e5", "e4"},
+                                                                            {"e4", "e3"},
+                                                                            {"e3", "e2"},
+                                                                            {"e2", "e1"},
+                                                                            {"e1", "d1"},
+                                                                            {"d1", "c1"},
+                                                                            {"c1", "b1"},
+                                                                            {"b1", "a1"},
+                                                                            {"a1", "a2"},
+                                                                            {"a2", "a3"},
+                                                                            {"a3", "a4"},
+                                                                            {"a4", "a5"},
+                                                                        };
         }
     }
 }
