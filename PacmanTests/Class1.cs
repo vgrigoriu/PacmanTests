6658120b451366c56f72357b46bdcef90c33079b
Victor Grigoriu <vgrigoriu@gmail.com>
2013-02-26 21:06:16 +0200

    Extract Pacman class

--- PacmanTests/Class1.cs
+++ PacmanTests/Class1.cs
@@ -9,11 +9,11 @@ namespace PacmanTests
         public void PacmanIsMovingCorrectlyAfterFirstTick()
         {
             var gameState = new GameState();
-            gameState.pacmanPosition = "a5";
+            gameState.pacman = new Pacman("a5", true);
 
-            MovePacman(gameState);
+            gameState.pacman.MovePacman();
 
-            Assert.Equal("a4", gameState.pacmanPosition);
+            Assert.Equal("a4", gameState.pacman.PacmanPosition);
         }
 
         [Fact]
@@ -31,22 +31,21 @@ namespace PacmanTests
         public void PacmanReversesDirectionAtEndOfForwardPath()
         {
             var gameState = new GameState();
-            gameState.pacmanPosition = "i1";
+            gameState.pacman = new Pacman("i1", true);
 
-            MovePacman(gameState);
-            Assert.False(gameState.pacmanIsMovingForward);
+            gameState.pacman.MovePacman();
+            Assert.False(gameState.pacman.PacmanIsMovingForward);
         }
 
         [Fact]
         public void WhenMovingBackwardsPacmanUsesBackwardPath()
         {
             var gameState = new GameState();
-            gameState.pacmanIsMovingForward = false;
-            gameState.pacmanPosition = "f5";
+            gameState.pacman = new Pacman("f5", false);
 
-            MovePacman(gameState);
+            gameState.pacman.MovePacman();
 
-            Assert.Equal(gameState.pacmanBackwardMoves["f5"], gameState.pacmanPosition);
+            Assert.Equal(Pacman.pacmanBackwardMoves["f5"], gameState.pacman.PacmanPosition);
         }
 
         [Fact]
@@ -83,36 +82,14 @@ namespace PacmanTests
             gameState.ghostIsToxic = (gameState.clockTick + 1) % gameState.toxicTickCount == 0;
         }
 
-        private void MovePacman(GameState gameState)
-        {
-            gameState.clockTick++;
-
-            var pacmanMoves = PacmanMoves(gameState);
-            if (!pacmanMoves.ContainsKey(gameState.pacmanPosition))
-            {
-                gameState.pacmanIsMovingForward = !gameState.pacmanIsMovingForward;
-                pacmanMoves = PacmanMoves(gameState);
-            }
-
-            gameState.pacmanPosition = pacmanMoves[gameState.pacmanPosition];
-        }
-
-        private static Dictionary<string, string> PacmanMoves(GameState gameState)
-        {
-            return gameState.pacmanIsMovingForward
-                       ? gameState.pacmanForwardMoves
-                       : gameState.pacmanBackwardMoves;
-        }
-
         private class GameState
         {
             public int clockTick = 0;
 
-            public string pacmanPosition;
             public string ghostPosition;
             public int toxicTickCount = 2;
             public bool ghostIsToxic;
-            public bool pacmanIsMovingForward = true;
+            public Pacman pacman;
 
             public Dictionary<string, string> ghostMoves = new Dictionary<string, string>()
                                                                {
@@ -142,53 +119,7 @@ namespace PacmanTests
                                                                    {"h1", "i1"},
                                                                };
 
-            public Dictionary<string, string> pacmanForwardMoves = new Dictionary<string, string>()
-                                                                {
-                                                                    {"a5", "a4"},
-                                                                    {"a4", "a3"},
-                                                                    {"a3", "a2"},
-                                                                    {"a2", "a1"},
-                                                                    {"a1", "b1"},
-                                                                    {"b1", "c1"},
-                                                                    {"c1", "d1"},
-                                                                    {"d1", "e1"},
-                                                                    {"e1", "e2"},
-                                                                    {"e2", "e3"},
-                                                                    {"e3", "e4"},
-                                                                    {"e4", "e5"},
-                                                                    {"e5", "f5"},
-                                                                    {"f5", "g5"},
-                                                                    {"g5", "h5"},
-                                                                    {"h5", "i5"},
-                                                                    {"i5", "i4"},
-                                                                    {"i4", "i3"},
-                                                                    {"i3", "i2"},
-                                                                    {"i2", "i1"},
-                                                                };
 
-            public Dictionary<string, string> pacmanBackwardMoves = new Dictionary<string, string>()
-                                                                        {
-                                                                            {"i1", "i2"},
-                                                                            {"i2", "i3"},
-                                                                            {"i3", "i4"},
-                                                                            {"i4", "i5"},
-                                                                            {"i5", "h5"},
-                                                                            {"h5", "g5"},
-                                                                            {"g5", "f5"},
-                                                                            {"f5", "e5"},
-                                                                            {"e5", "e4"},
-                                                                            {"e4", "e3"},
-                                                                            {"e3", "e2"},
-                                                                            {"e2", "e1"},
-                                                                            {"e1", "d1"},
-                                                                            {"d1", "c1"},
-                                                                            {"c1", "b1"},
-                                                                            {"b1", "a1"},
-                                                                            {"a1", "a2"},
-                                                                            {"a2", "a3"},
-                                                                            {"a3", "a4"},
-                                                                            {"a4", "a5"},
-                                                                        };
         }
     }
 }
---
+++ PacmanTests/Pacman.cs
@@ -0,0 +1,86 @@
+﻿
+using System.Collections.Generic;
+
+namespace PacmanTests
+{
+    public class Pacman
+    {
+        #region Dictionaries
+        public static Dictionary<string, string> pacmanForwardMoves = new Dictionary<string, string>()
+                                                                          {
+                                                                              {"a5", "a4"},
+                                                                              {"a4", "a3"},
+                                                                              {"a3", "a2"},
+                                                                              {"a2", "a1"},
+                                                                              {"a1", "b1"},
+                                                                              {"b1", "c1"},
+                                                                              {"c1", "d1"},
+                                                                              {"d1", "e1"},
+                                                                              {"e1", "e2"},
+                                                                              {"e2", "e3"},
+                                                                              {"e3", "e4"},
+                                                                              {"e4", "e5"},
+                                                                              {"e5", "f5"},
+                                                                              {"f5", "g5"},
+                                                                              {"g5", "h5"},
+                                                                              {"h5", "i5"},
+                                                                              {"i5", "i4"},
+                                                                              {"i4", "i3"},
+                                                                              {"i3", "i2"},
+                                                                              {"i2", "i1"},
+                                                                          };
+
+        public static Dictionary<string, string> pacmanBackwardMoves = new Dictionary<string, string>()
+                                                                           {
+                                                                               {"i1", "i2"},
+                                                                               {"i2", "i3"},
+                                                                               {"i3", "i4"},
+                                                                               {"i4", "i5"},
+                                                                               {"i5", "h5"},
+                                                                               {"h5", "g5"},
+                                                                               {"g5", "f5"},
+                                                                               {"f5", "e5"},
+                                                                               {"e5", "e4"},
+                                                                               {"e4", "e3"},
+                                                                               {"e3", "e2"},
+                                                                               {"e2", "e1"},
+                                                                               {"e1", "d1"},
+                                                                               {"d1", "c1"},
+                                                                               {"c1", "b1"},
+                                                                               {"b1", "a1"},
+                                                                               {"a1", "a2"},
+                                                                               {"a2", "a3"},
+                                                                               {"a3", "a4"},
+                                                                               {"a4", "a5"},
+                                                                           };
+        #endregion
+
+        public Pacman(string pacmanPosition, bool isMovingForward)
+        {
+            PacmanIsMovingForward = isMovingForward;
+            PacmanPosition = pacmanPosition;
+        }
+
+        public string PacmanPosition { get; private set; }
+        public bool PacmanIsMovingForward { get; private set; }
+
+        public void MovePacman()
+        {
+            var pacmanMoves = PacmanMoves();
+            if (!pacmanMoves.ContainsKey(PacmanPosition))
+            {
+                PacmanIsMovingForward = !PacmanIsMovingForward;
+                pacmanMoves = PacmanMoves();
+            }
+
+            PacmanPosition = pacmanMoves[PacmanPosition];
+        }
+
+        private Dictionary<string, string> PacmanMoves()
+        {
+            return PacmanIsMovingForward
+                       ? pacmanForwardMoves
+                       : pacmanBackwardMoves;
+        }
+    }
+}
