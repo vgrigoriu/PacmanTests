eb34f1039ab75cc9b92e6433cc9fb9c36e3f153d
Victor Grigoriu <vgrigoriu@gmail.com>
2013-02-26 19:11:14 +0200

    First commit

---
+++ PacmanTests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace PacmanTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestOnePlusOne()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+
+
+    }
+}
